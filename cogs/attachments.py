5edc0d87869739bf1c1ab8fd78cf65e60a41d80e
nissy69 <62857665+nissy69@users.noreply.github.com>
2020-03-30 00:23:57 +0200

    Update attachments.py

--- cogs/attachments.py
+++ cogs/attachments.py
@@ -6,10 +6,10 @@ class attachments(commands.Cog):
         self.bot = bot
     @commands.Cog.listener()
     async def on_message(self, message):
-        channel = self.bot.get_channel(690584660123582494)
+        channel = self.bot.get_channel(673135393616691211)
         if not message.channel.category_id:
             return
-        if not message.channel.category_id == 553232325832343574:
+        if not message.channel.category_id == 622343159439163394:
             return
         if message.attachments:
             for i in message.attachments:
